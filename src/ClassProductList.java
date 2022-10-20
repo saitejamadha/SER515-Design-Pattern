0d2e81366747ce3cd1b90a52be19166fddcd33e0
Sai Teja Madha <smadha1@asu.edu>
2022-10-20 01:36:51 -0700

    add few items to List

--- src/ClassProductList.java
+++ src/ClassProductList.java
@@ -1,13 +1,27 @@
 import java.util.ArrayList;
 
-public class ClassProductList extends ArrayList {
+public class ClassProductList extends ArrayList<Product> {
 
 	private final ProductIterator productIterator;
 	private ReminderVisitor reminderVisitor;
-	private Product[] product;
 
 	public ClassProductList() {
 		this.productIterator = new ProductIterator(this);
+
+		// Construct a new arraylist with 7 products in it.
+		Product product1 = new Product();
+		Product product2 = new Product();
+		Product product3 = new Product();
+		Product product4 = new Product();
+		Product product5 = new Product();
+
+		this.addAll( new ArrayList<>() {{
+			add(product1);
+			add(product2);
+			add(product3);
+			add(product4);
+			add(product5);
+		}});
 	}
 
 	void accept(NodeVisitor visitor) {
--- src/OfferingList.java
+++ src/OfferingList.java
@@ -1,13 +1,27 @@
 import java.util.ArrayList;
 
-public class OfferingList extends ArrayList {
+public class OfferingList extends ArrayList<Offering> {
 
 	private final OfferingIterator offeringIterator;
 	private Trading trading;
-	private Offering[] offering;
 
 	public OfferingList() {
 		this.offeringIterator = new OfferingIterator(this);
+
+		// Add 5 offerings to this OfferingList.
+		Offering offering1 = new Offering();
+		Offering offering2 = new Offering();
+		Offering offering3 = new Offering();
+		Offering offering4 = new Offering();
+		Offering offering5 = new Offering();
+
+		this.addAll( new ArrayList<>(){{
+			add(offering1);
+			add(offering2);
+			add(offering3);
+			add(offering4);
+			add(offering5);
+		}});
 	}
 
 	/**
