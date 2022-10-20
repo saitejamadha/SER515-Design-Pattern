6e51ad778c2fe09a931e76fc94d1d4d3ac89cc0a
Sai Teja Madha <smadha1@asu.edu>
2022-10-20 01:15:21 -0700

    implemented offering iterator

--- src/ClassProductList.java
+++ src/ClassProductList.java
@@ -2,11 +2,23 @@ import java.util.ArrayList;
 
 public class ClassProductList extends ArrayList {
 
-	private ProductIterator productIterator;
+	private final ProductIterator productIterator;
 	private ReminderVisitor reminderVisitor;
 	private Product[] product;
 
+	public ClassProductList() {
+		this.productIterator = new ProductIterator(this);
+	}
+
 	void accept(NodeVisitor visitor) {
+	}
+
+	/**
+	 * Return an iterator for the ClassProductList.
+	 * @return 	An instance of the iterator.
+	 */
+	public ListIterator getIterator() {
+		return this.productIterator;
 	}
 
 }
--- src/OfferingList.java
+++ src/OfferingList.java
@@ -2,8 +2,20 @@ import java.util.ArrayList;
 
 public class OfferingList extends ArrayList {
 
-	private OfferingIterator offeringIterator;
+	private final OfferingIterator offeringIterator;
 	private Trading trading;
 	private Offering[] offering;
+
+	public OfferingList() {
+		this.offeringIterator = new OfferingIterator(this);
+	}
+
+	/**
+	 * Return an iterator for the OfferingList.
+	 * @return 	An instance of the iterator.
+	 */
+	public ListIterator getIterator() {
+		return this.offeringIterator;
+	}
 
 }
